fe56b1224f41564f6388923abcab3e70047c11b3
Kayan Galvani <kayan.galvani30@gmail.com>
2023-08-18 09:26:07 -0300

    first commit

---
+++ .GIT HUB/Tuturial Atulizar.py
@@ -0,0 +1,9 @@
+#git status - Conferir quais mundaças foram feitas
+
+#git add . - Adicionar mundanças ao git
+
+#git commit - m "..." - Dar nome a atualização
+
+#git pull origin main
+#git push origin main
+                        #FINALIZAR ATUALIZAÇÕES
