f4f3e78f13a6e304618e93a3529ab8595b59d497
Kayan Galvani <kayan.galvani30@gmail.com>
2023-08-18 09:29:12 -0300

    atvd 01

--- Atividades/script1.py
+++ Atividades/script1.py
@@ -1,1 +1,36 @@
 print(".")
+#    Complete o código do exemplo abordado nos slides, desenvolvendo as opções Editar e Sair;
+#   Desenvolva três exemplos de códigos em Python (.py), demonstrando diferentes tipos de funções. Crie uma função sem parâmetros e sem retorno, outra função com parâmetros e sem retorno, e uma terceira função com parâmetros e retorno. Seja criativo e explore a versatilidade das funções em Python. Certifique-se de fornecer comentários explicativos em seu código para facilitar a compreensão;
+
+
+#01
+def msg():
+    print("(01) Função simples")
+
+msg()
+print('//////////////////////////////////////////////////////////////')
+#//////////////////////////////////////////////////////////////////////////
+
+#02
+def number(numero):
+    if numero % 2 == 0:
+        print(f"(02) O número {numero} é par.")
+    else:
+        print(f"(02) O número {numero} é ímpar.")
+
+number(10)
+number(7)
+print('//////////////////////////////////////////////////////////////')
+#//////////////////////////////////////////////////////////////////////////
+#03
+def calcularTotal(precoUnitario, quantidade):
+    total = precoUnitario * quantidade
+    return total
+
+preco = 10
+quantidade = 7
+
+valorTotal = calcularTotal(preco, quantidade)
+print(f"(03) O valor total a ser pago é: R$ {valorTotal:.2f}")
+print('//////////////////////////////////////////////////////////////')
+#//////////////////////////////////////////////////////////////////////////
